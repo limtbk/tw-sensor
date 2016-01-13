676e755c4c92c72cbf2a6766b3352ebe5fc7afcc
lim <ivan.klimchuk@gmail.com>
2016-01-13 05:38:06 +0200

    Version 1.3

--- firmware/main.c
+++ firmware/main.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/eeprom.h>
 #include "ports.h"
 #include "usart.h"
 #include "macro.h"
@@ -18,81 +19,51 @@
 #define SSR11 PORTA_D10
 #define SSRT PORTA_D13 //PORT_B5
 
+#define DBM10CTRL PORTA_A3
+
 #define MEASURES 32
 
 #define measure_pinm(__port, __ddr, __pin, __nn, __portt, __ddrt, __pint, __nnt) \
-		uint8_t tmp = 0;\
-		asm volatile (\
-				"	cli"							"\n\t"\
-				"	cbi %[portt],%[nnt]"			"\n\t" /*PORTT=0*/\
-				"	cbi %[port],%[nn]"				"\n\t" /*PORT=0*/\
-				"	sbi %[ddr],%[nn]"				"\n\t" /*DDR=1*/\
-				"	cbi %[ddr],%[nn]"				"\n\t" /*DDR=0*/\
-				"	cbi %[port],%[nn]"				"\n\t" /*PORT=0*/\
-				"	sbi %[portt],%[nnt]"			"\n\t" /*PORTT=1*/\
-				"	rjmp mloop11%="					"\n\t"\
-				"mloop10%=:"						"\n\t"\
-				"	subi %[t],0xFF"					"\n\t" /*t+=1*/\
-				"mloop11%=:"						"\n\t"\
-				"	sbis %[pin],%[nn]"				"\n\t" /*PIN*/\
-				"	rjmp mloop10%="					"\n\t"\
-				"	sbi %[portt],%[nnt]"			"\n\t" /*PORTT=1*/\
-				"	sbi %[port],%[nn]"				"\n\t" /*PORT=1*/\
-				"	sbi %[ddr],%[nn]"				"\n\t" /*DDR=1*/\
-				"	cbi %[ddr],%[nn]"				"\n\t" /*DDR=0*/\
-				"	cbi %[port],%[nn]"				"\n\t" /*PORT=0*/\
-				"	cbi %[portt],%[nnt]"			"\n\t" /*PORTT=0*/\
-				"	rjmp mloop21%="					"\n\t"\
-				"mloop20%=:"						"\n\t"\
-				"	subi %[t],0xFF"					"\n\t" /*t+=1*/\
-				"mloop21%=:"						"\n\t"\
-				"	sbic %[pin],%[nn]"				"\n\t" /*PIN*/\
-				"	rjmp mloop20%="					"\n\t"\
-				"	sei"							"\n\t"\
-				:\
-				[t] "+d" (tmp)\
-				:\
-				[port] "I" (_SFR_IO_ADDR(__port)),\
-				[ddr] "I" (_SFR_IO_ADDR(__ddr)),\
-				[pin] "I" (_SFR_IO_ADDR(__pin)),\
-				[nn] "I" (__nn),\
-				[portt] "I" (_SFR_IO_ADDR(__portt)),\
-				[nnt] "I" (__nnt)\
-		);
+	uint8_t tmp = 0;\
+	asm volatile (\
+		"	cli"							"\n\t"\
+		"	cbi %[portt],%[nnt]"			"\n\t" /*PORTT=0*/\
+		"	cbi %[port],%[nn]"				"\n\t" /*PORT=0*/\
+		"	sbi %[ddr],%[nn]"				"\n\t" /*DDR=1*/\
+		"	cbi %[ddr],%[nn]"				"\n\t" /*DDR=0*/\
+		"	cbi %[port],%[nn]"				"\n\t" /*PORT=0*/\
+		"	sbi %[portt],%[nnt]"			"\n\t" /*PORTT=1*/\
+		"	rjmp mloop11%="					"\n\t"\
+		"mloop10%=:"						"\n\t"\
+		"	subi %[t],0xFF"					"\n\t" /*t+=1*/\
+		"mloop11%=:"						"\n\t"\
+		"	sbis %[pin],%[nn]"				"\n\t" /*PIN*/\
+		"	rjmp mloop10%="					"\n\t"\
+		"	sbi %[portt],%[nnt]"			"\n\t" /*PORTT=1*/\
+		"	sbi %[port],%[nn]"				"\n\t" /*PORT=1*/\
+		"	sbi %[ddr],%[nn]"				"\n\t" /*DDR=1*/\
+		"	cbi %[ddr],%[nn]"				"\n\t" /*DDR=0*/\
+		"	cbi %[port],%[nn]"				"\n\t" /*PORT=0*/\
+		"	cbi %[portt],%[nnt]"			"\n\t" /*PORTT=0*/\
+		"	rjmp mloop21%="					"\n\t"\
+		"mloop20%=:"						"\n\t"\
+		"	subi %[t],0xFF"					"\n\t" /*t+=1*/\
+		"mloop21%=:"						"\n\t"\
+		"	sbic %[pin],%[nn]"				"\n\t" /*PIN*/\
+		"	rjmp mloop20%="					"\n\t"\
+		"	sei"							"\n\t"\
+		:\
+		[t] "+d" (tmp)\
+		:\
+		[port] "I" (_SFR_IO_ADDR(__port)),\
+		[ddr] "I" (_SFR_IO_ADDR(__ddr)),\
+		[pin] "I" (_SFR_IO_ADDR(__pin)),\
+		[nn] "I" (__nn),\
+		[portt] "I" (_SFR_IO_ADDR(__portt)),\
+		[nnt] "I" (__nnt)\
+	);
 
 #define measure_pin(port, portt) measure_pinm(port, portt)
-
-uint16_t timer1_lastTime;
-
-/*
-void timer1_init(void) {
-#if defined (__AVR_ATmega32U4__)
-	TCCR1A = (0<<COM1A1)|(0<<COM1A0)|(0<<COM1B1)|(0<<COM1B0)|(0<<COM1C1)|(0<<COM1C0)|(0<<WGM11)|(0<<WGM10);
-	TCCR1B = (0<<ICNC1)|(0<<ICES1)|(0<<WGM13)|(0<<WGM12)|(0<<CS12)|(0<<CS11)|(1<<CS10); //Normal, clk/1
-	TCCR1C = (0<<FOC1A)|(0<<FOC1B)|(0<<FOC1C);
-	TIMSK1 = (0<<ICIE1)|(0<<OCIE1C)|(0<<OCIE1B)|(0<<OCIE1A)|(0<<TOIE1);
-#elif defined (__AVR_ATmega328P__)
-	TCCR1A = (0<<COM1A1)|(0<<COM1A0)|(0<<COM1B1)|(0<<COM1B0)|(0<<WGM11)|(0<<WGM10);
-	TCCR1B = (0<<ICNC1)|(0<<ICES1)|(0<<WGM13)|(0<<WGM12)|(0<<CS12)|(0<<CS11)|(1<<CS10); //Normal, clk/1
-	TIMSK0 = (0<<ICIE1)|(0<<OCIE1B)|(0<<OCIE1A)|(0<<TOIE1);
-#else
-	TCCR1A = (0<<COM1A1)|(0<<COM1A0)|(0<<COM1B1)|(0<<COM1B0)|(0<<WGM11)|(0<<WGM10);
-	TCCR1B = (0<<ICNC1)|(0<<ICES1)|(0<<WGM13)|(0<<WGM12)|(0<<CS12)|(0<<CS11)|(1<<CS10); //Normal, clk/1
-	TIMSK = (0<<TICIE1)|(0<<OCIE1B)|(0<<OCIE1A)|(0<<TOIE1);
-#endif
-}
-
-void resetTimer1(void) {
-	TCNT1 = 0;
-}
-
-uint16_t timer1_measureTime(void) {
-	uint16_t time = TCNT1;
-	uint16_t result = time - timer1_lastTime;
-	timer1_lastTime = time;
-	return result;
-}
-*/
 
 void init(void) {
 
@@ -106,38 +77,16 @@ void init(void) {
 	SETD(SSRT);
 	CLRP(SSRT);
 
+	SETP(DBM10CTRL); //set DBM10 to serial port mode
+	SETD(DBM10CTRL);
+
 #ifdef __AVR_ATmega32U4__
 	USBCON = (0<<USBE)|(1<<FRZCLK)|(0<<OTGPADE)|(0<<VBUSTE);
 #endif
 
 	usart_init();
-//	timer1_init();
 	sei();
 }
-
-//uint16_t measure_for_pin(volatile uint8_t *pin, uint8_t pinn)
-//{
-//	uint16_t result = 0;
-//	uint8_t pinmask = 1<<pinn;
-//	cli();
-//	timer1_measureTime();
-//	for (uint8_t i = 0; i < MEASURES; i++) {
-//		CLRP(SSRT);
-//		while (((*pin) & pinmask) != 0) {}
-//		*(pin+1) |= pinmask; //DDR = 1
-//		*(pin+2) &= ~pinmask; //PORT = 0
-//		*(pin+1) &= ~pinmask; //DDR = 0
-//		SETP(SSRT);
-//		while (!(((*pin) & pinmask) != 0)) {}
-//		*(pin+1) |= pinmask; //DDR = 1
-//		*(pin+2) |= pinmask; //PORT = 1
-//		*(pin+1) &= ~pinmask; //DDR = 0
-//		*(pin+2) &= ~pinmask; //PORT = 0
-//	}
-//	result = timer1_measureTime();
-//	sei();
-//	return result;
-//}
 
 uint16_t measure_for_ssr0()
 {
@@ -178,7 +127,6 @@ uint16_t measure_for_ssr3()
 	}
 	return result;
 }
-
 
 uint16_t measure_for_ssr4()
 {
@@ -260,13 +208,28 @@ uint16_t measure_for_ssr11()
 	return result;
 }
 
+void reboot() {
+#ifdef __AVR_ATmega328P__
+	void *bl = (void *)0x3c00; //for ATMega328 with bootloader
+	goto *bl; //Yep, its hack to reboot without watchdog
+#endif
+}
+
 void print_version()
 {
-	usart_printstr("V1.2\n\r");
+	usart_printstr("V1.3\n\r");
 }
 
 int main(void)
 {
+	eeprom_busy_wait();
+	uint8_t reboots = eeprom_read_byte(0);
+	if (reboots) {
+		eeprom_write_byte(0, reboots-1);
+		eeprom_busy_wait();
+		reboot();
+	}
+
 	init();
 
 	usart_printstr("\n\rCapacity sensor\n\r");
@@ -275,8 +238,10 @@ int main(void)
 	uint16_t tarr[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	uint16_t minarr[12] = {UINT16_MAX, UINT16_MAX, UINT16_MAX, UINT16_MAX, UINT16_MAX, UINT16_MAX, UINT16_MAX, UINT16_MAX, UINT16_MAX, UINT16_MAX, UINT16_MAX, UINT16_MAX};
 	uint16_t maxarr[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	uint8_t cm = 0;
-	uint8_t m = 0;
+	uint8_t contmeasure = 0;
+	uint8_t measure = 0;
+	uint8_t rawmode = 0;
+	uint8_t binmode = 0;
 	uint8_t cal_min_cycles = 255;
 	uint8_t cal_max_cycles = 255;
 
@@ -299,7 +264,8 @@ int main(void)
 										"t - hex text num mode (default)\n\r"
 										"w - raw data mode\n\r"
 										"l - normalized data mode (default)\n\r"
-										"v - version\n\r");
+										"v - version\n\r"
+										"f - wait for firmware next 10 reboots\n\r");
 						break;
 					}
 				case 'v':
@@ -309,17 +275,17 @@ int main(void)
 					}
 				case 'm':
 					{
-						m = 1;
+						measure = 1;
 						break;
 					}
 				case 'c':
 					{
-						cm = 1;
+						contmeasure = 1;
 						break;
 					}
 				case 's':
 					{
-						cm = 0;
+						contmeasure = 0;
 						break;
 					}
 				case 'r':
@@ -337,13 +303,38 @@ int main(void)
 					}
 				case 'R':
 					{
-						void *bl = (void *)0x3c00; //for ATMega328 with bootloader
-						goto *bl;
+						reboot();
 						break;
 					}
 				case 'x':
 					{
 						cal_max_cycles = 255;
+						break;
+					}
+				case 'w':
+					{
+						rawmode = 1;
+						break;
+					}
+				case 'l':
+					{
+						rawmode = 0;
+						break;
+					}
+				case 'b':
+					{
+						binmode = 1;
+						break;
+					}
+				case 't':
+					{
+						binmode = 0;
+						break;
+					}
+				case 'f':
+					{
+						eeprom_write_byte(0, 10);
+						eeprom_busy_wait();
 						break;
 					}
 				default:
@@ -378,27 +369,44 @@ int main(void)
 			cal_max_cycles--;
 		}
 
-		if (m || cm) {
+		if (measure || contmeasure) {
+			usart_putchr('S'); //Start symbol
+			uint8_t ctrlbyte = 0;
 			for (uint8_t ti = 0; ti < 12; ti++) {
 				uint16_t tt = tarr[ti];
 
-				if (minarr[ti]<UINT16_MAX) {
-					if ((maxarr[ti]>0) && (minarr[ti]<maxarr[ti])) {
-						uint16_t delta = maxarr[ti] - minarr[ti];
-						uint16_t td = (tarr[ti]>minarr[ti])?(tarr[ti]-minarr[ti]):0;
-						uint32_t tmp = ((int32_t)td*255)/delta;
-						tt = (tmp<255)?(uint16_t)tmp:255;
-					} else {
-						tt = (tarr[ti]>minarr[ti])?(tarr[ti]-minarr[ti]):0;
+				if (!rawmode) {
+					if (minarr[ti]<UINT16_MAX) {
+						if ((maxarr[ti]>0) && (minarr[ti]<maxarr[ti])) {
+							uint16_t delta = maxarr[ti] - minarr[ti];
+							uint16_t td = (tarr[ti]>minarr[ti])?(tarr[ti]-minarr[ti]):0;
+							uint32_t tmp = ((int32_t)td*255)/delta;
+							tt = (tmp<255)?(uint16_t)tmp:255;
+						} else {
+							tt = (tarr[ti]>minarr[ti])?(tarr[ti]-minarr[ti]):0;
+						}
 					}
 				}
-
-				usart_printhex(HI(tt));
-				usart_printhex(LO(tt));
-				usart_putchr(' ');
+				ctrlbyte ^= HI(tt);
+				ctrlbyte ^= LO(tt);
+				if (!binmode) {
+					usart_printhex(HI(tt));
+					usart_printhex(LO(tt));
+					usart_putchr(' ');
+				} else {
+					usart_putchr(HI(tt));
+					usart_putchr(LO(tt));
+				}
 			}
-			usart_printstr("\n\r");
-			m = 0;
+			if (!binmode) {
+				usart_printhex(ctrlbyte);
+				usart_putchr('N'); //End symbol
+				usart_printstr("\n\r");
+			} else {
+				usart_putchr(ctrlbyte);
+				usart_putchr('N'); //End symbol
+			}
+			measure = 0;
 		}
 	}
 
